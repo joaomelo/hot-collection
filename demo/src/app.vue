311d85e31cec9005b691c91b45c6c430b22a0638
joao <j@vmelo.net>
2020-03-03 15:34:37 -0600

    plugged firebase and fireauth-machine

--- demo/src/app.vue
+++ demo/src/app.vue
@@ -1,27 +1,23 @@
 <template>
   <v-app>
     <v-content>
-      <v-container>
-        <v-row>
-          <v-col cols="3" />
-          <v-col
-            cols="6"
-            class="grey lighten-5"
-          >
-            <Demo />
-          </v-col>
-          <v-col cols="3" />
-        </v-row>
-      </v-container>
+      <p>Demo<p>
     </v-content>
+    <v-overlay :value="overlay">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>  
   </v-app>
 </template>
 
 <script>
-import Demo from './demo';
+import {authMachine} from './services';
 
 export default {
   name: 'App',
-  components: { Demo }
+  computed: {
+    overlay(){
+      return authMachine.status === 'UNSOLVED';
+    }
+  }
 };
 </script>
---
+++ demo/src/services.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { AuthMachine } from '@joaomelo/fireauth-machine';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+const fireapp = firebase.initializeApp(firebaseConfig);
+const firedb = fireapp.firestore();
+
+//init auth
+const fireauth = fireapp.auth();
+const authMachine = new AuthMachine(fireauth);
+Vue.observable(authMachine);
+//log the default user
+authMachine.service.signInWithEmailAndPassword(process.env.USER, process.env.PASS);
+
+export { firebase, fireapp, firedb, authMachine };
