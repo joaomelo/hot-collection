aabb6069728acabb269943fddea2b7e35774aa9d
joao <j@vmelo.net>
2020-05-04 18:31:51 -0300

    basic operations working without options

---
+++ demo/src/examples/del-item.js
@@ -0,0 +1,31 @@
+import HotCollection from '__lib'; // '@joaomelo/hot-collection';
+import { byId, query } from '../helpers';
+
+function renderDelItemExample (examplesEl, adapter) {
+  const sectionEl = document.createElement('section');
+  sectionEl.innerHTML = `
+    <h3 class="example-title">Example: Del Employee</h3>
+    <div id="del-item-example" class="example-content"></div>
+    `;
+  examplesEl.appendChild(sectionEl);
+  renderDelItem(byId('del-item-example'), adapter);
+};
+
+function renderDelItem (el, adapter) {
+  const employeeCollection = new HotCollection('employees', { adapter });
+
+  employeeCollection.subscribe(employees => {
+    el.innerHTML = employees.length <= 0
+      ? '<p>no employees found</p>'
+      : `<ul>${employees.reduce((a, e) => a + `${renderListItem(e)}`, '')}</ul>`;
+    query('.btn-del-item-del').forEach(btn => { btn.onclick = () => employeeCollection.del(btn.dataset.id); });
+  });
+};
+
+function renderListItem (e) {
+  const btn = `<button data-id="${e.id}" class="btn-del-item-del" type="button">Del</button>`;
+  const li = `<li>${e.name}:${e.dpto || 'no dpto'}${btn}</li>`;
+  return li;
+};
+
+export { renderDelItemExample };
--- demo/src/examples/index.js
+++ demo/src/examples/index.js
@@ -1,8 +1,8 @@
 import { renderListItemsExample } from './list-items';
 import { renderAddItemExample } from './add-item';
 import { renderSetItemExample } from './set-item';
+import { renderDelItemExample } from './del-item';
 // import { renderSubExample } from './examples/sub';
-// import { renderDelExample } from './examples/del';
 // import { renderFilterBackendExample } from './examples/filter-backend';
 // import { renderFilterClientExample } from './examples/filter-client';
 
@@ -11,6 +11,7 @@ function renderExamples (examplesEl, adapter) {
   renderListItemsExample(examplesEl, adapter);
   renderAddItemExample(examplesEl, adapter);
   renderSetItemExample(examplesEl, adapter);
+  renderDelItemExample(examplesEl, adapter);
 }
 
 export { renderExamples };
